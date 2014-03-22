e48fab9c14b51c3f55dd4e8d65c590a4a26db11c
leeight <leeight@gmail.com>
2014-03-22 21:31:57 +0800

    ecomfe/edp#184

--- test/path.spec.js
+++ test/path.spec.js
@@ -20,8 +20,36 @@ describe('path', function (){
     it('default', function(){
         expect( path.satisfy( 'er/main', 'er/*' ) ).toBe( true );
         expect( path.satisfy( 'er/main/123', 'er/*' ) ).toBe( false );
-        // FIXME(user)
-        // expect( path.satisfy( 'er/main/123', 'er/**' ) ).toBe( true );
+        expect( path.satisfy( '123/er/main/123', '/er/*/*' ) ).toBe( false );
+
+        expect( path.satisfy( '/er/main/123', '/er/*/*' ) ).toBe( true );
+        expect( path.satisfy( 'er/main/123', '/er/*/*' ) ).toBe( false );
+        expect( path.satisfy( 'er/main/123', '!er/**' ) ).toBe( false );
+        expect( path.satisfy( 'er/main/123', 'er/**' ) ).toBe( true );
+        expect( path.satisfy( 'er/main/123/1/1/1/1/1', 'er/**' ) ).toBe( true );
+        expect( path.satisfy( 'er/main/123', 'er/*' ) ).toBe( false );
+        expect( path.satisfy( 'er', 'er/*' ) ).toBe( false );
+
+        expect( path.satisfy( 'dep/esf-ms/1.0.2/src/copyright.less', '*.less' ) ).toBe( true );
+        expect( path.satisfy( 'dep/ef/3.1.0-alpha.3/demo/chi/server/affair.js', '/dep/*/*/demo' ) ).toBe( false );
+        expect( path.satisfy( 'dep/est/1.1.0/example/prettify/lang-scala.js', 'prettify/*.js' ) ).toBe( false );
+        expect( path.satisfy( 'dep/est/1.1.0/example/prettify/lang-scala.js', '**/prettify/*.js' ) ).toBe( true );
+
+        // A leading "**"
+        expect( path.satisfy( 'foo', '**/foo' ) ).toBe( true );
+        expect( path.satisfy( '123/foo', '**/foo' ) ).toBe( true );
+        expect( path.satisfy( '123/foo/456', '**/foo' ) ).toBe( false );
+
+        // A trailing "/**"
+        expect( path.satisfy( 'abc', 'abc/**' ) ).toBe( false );
+        expect( path.satisfy( 'abc/123', 'abc/**' ) ).toBe( true );
+        expect( path.satisfy( 'abc/123/456', 'abc/**' ) ).toBe( true );
+        expect( path.satisfy( '123/abc/456', 'abc/**' ) ).toBe( false );
+
+        // A slash followed by two consecutive asterisks
+        expect( path.satisfy( 'a/b', 'a/**/b' ) ).toBe( true );
+        expect( path.satisfy( 'a/123/b', 'a/**/b' ) ).toBe( true );
+        expect( path.satisfy( 'a/123/456/b', 'a/**/b' ) ).toBe( true );
     });
 });
 
